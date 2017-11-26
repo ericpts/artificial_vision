934a67f81646cc7ee1486486d98c14f62ee830ea
Eric P. Stavarache <ericptst@gmail.com>
2017-11-26 12:03:36 +0200

    * Add options for removing a fixed object instead of mouse selection. * Speed up dyamic programming algorithm. * Cache google images for future usage. * Almost working task 1.5.

--- Proiect2/generate_pdf.py
+++ Proiect2/generate_pdf.py
@@ -6,6 +6,7 @@ import argparse
 import subprocess
 import tempfile
 import random
+import urllib
 import multiprocessing as mp
 from pathlib import Path
 from reportlab.pdfgen import canvas
@@ -13,70 +14,177 @@ from reportlab.pdfgen import canvas
 sys.path.append('../')
 from util import read_image
 
+from scipy import misc
 from lib_google_img.google_images_download import google_images_download
 
 def add_picture_to_canvas(pic, desc, c):
     (w, h) = read_image(pic).shape[0:2]
-    c.setPageSize((h, w + 100))
+    c.setPageSize((max(h, 500), w + 100))
     c.drawInlineImage(str(pic), 0, 0)
     c.drawCentredString(h / 2, w + 50, desc)
     c.showPage()
 
-def gen_with_args(c, tmp_dir, data_dir, pic_name, desc, args):
-
-    target = str(data_dir / '{}.jpg'.format(pic_name))
-
-    if not exists(target):
-        target = str(data_dir / '{}.jpeg'.format(pic_name))
-
-    output = str(tmp_dir / '{}_out.jpg'.format(pic_name))
+def gen_output(target, output, args):
     subprocess.check_call(
             ['python3', 'main.py',
                 target, output,
                 *args])
-    add_picture_to_canvas(output, desc, c)
+
+def add_generated_to_canvas(c, target, output, desc, imresize_fn):
+    add_picture_to_canvas(target, desc + ': initial picture', c)
+
+    if imresize_fn:
+        imresize_output = str(tmp_dir / 'imresize_out.jpg')
+        misc.imsave(imresize_output, imresize_fn(read_image(target)))
+        add_picture_to_canvas(imresize_output, desc + ': modified with imresize', c)
+
+    add_picture_to_canvas(output, desc + ': modified with paper algorithm', c)
+
+def add_from_data(c, tmp_dir, target, output, desc, args, imresize_fn=None):
+    gen_output(target, output, args)
+    add_generated_to_canvas(c, target, output, desc, imresize_fn)
+
+def add_from_name(c, tmp_dir, data_dir, pic_name, desc, args, imresize_fn=None):
+
+    target = str(data_dir / '{}.jpg'.format(pic_name))
+    if not exists(target):
+        target = str(data_dir / '{}.jpeg'.format(pic_name))
+
+    output = str(tmp_dir / '{}_out.jpg'.format(pic_name))
+
+    add_from_data(c, tmp_dir, target, output, desc, args, imresize_fn)
+
+def delta_imresize(lines, columns):
+    def fn(img):
+        (n, m) = img.shape[0:2]
+        return misc.imresize(img, (n + lines, m + columns))
+    return fn
 
 def add_task_1_1(c, tmp_dir, data_dir):
     print('Generating task 1.1')
-    gen_with_args(c, tmp_dir, data_dir,
+    add_from_name(c, tmp_dir, data_dir,
             'castel', 'Castel.jpg with -50 columns',
-            ['--columns', '-50'])
+            ['--columns', '-50'], delta_imresize(0, -50))
 
 def add_task_1_2(c, tmp_dir, data_dir):
     print('Generating task 1.2')
-    gen_with_args(c, tmp_dir, data_dir,
+    add_from_name(c, tmp_dir, data_dir,
             'praga', 'Praga.jpg with -100 lines',
-            ['--lines', '-100'])
+            ['--lines', '-100'], delta_imresize(-100, 0))
 
 def add_task_1_3(c, tmp_dir, data_dir):
     print('Generating task 1.3')
-    gen_with_args(c, tmp_dir, data_dir,
+    add_from_name(c, tmp_dir, data_dir,
             'delfin', 'Delfin.jpg with +50 lines and +50 columns',
-            ['--lines', '50', '--columns', '50'])
+            ['--lines', '50', '--columns', '50'], delta_imresize(50, 50))
+
+def add_task_1_4(c, tmp_dir, data_dir):
+    print('Generating task 1.4')
+    add_from_name(c, tmp_dir, data_dir,
+            'lac', 'Lac.jpg with the little girl removed',
+            ['--remove-object-fixed-coordinates', *[str(x) for x in [168, 397, 205, 430]]])
+
+def add_task_1_5(c, tmp_dir, data_dir):
+    print('Generating task 1.5')
+
+    keywords = [
+            'cat',
+            'guitar',
+            'starry night',
+            'rose',
+            'cote d\'ivore',
+    ]
+
+    def random_modification(n, m):
+        def var(x):
+            def n():
+                return x - int(random.normalvariate(x, x/20))
+            r = n()
+            while r <= -x or r >= x:
+                r = n()
+            return r
+
+        return (var(n), var(m))
+
+    LIMIT = 10
+    def process_fn(keyword, target, output):
+        def ensure_target_exists():
+            if Path(target).exists():
+                return
+            links = google_images_download.get_image_links(
+                    search_keywords=[keyword], keywords=['high resolution'], requests_delay=0, limit=LIMIT)
+            for target_link in links:
+                try:
+                    print('Trying to retrieve target link {}'.format(target_link))
+                    misc.imsave(target, read_image(target_link))
+                    break
+                except urllib.error.HTTPError:
+                    continue
+                except OSError:
+                    continue
+
+        ensure_target_exists()
+        target_img = read_image(target)
+        (n, m) = target_img.shape[0:2]
+        (del_lines, del_columns) = random_modification(n, m)
+
+        args = [
+            '--lines', str(del_lines),
+            '--columns', str(del_columns)
+            ]
+        gen_output(target, output, args)
+
+    workers = []
+
+    worker_files = []
+    for (at, keyword) in enumerate(keywords):
+        target = str(tmp_dir / '1_5_{}_in.jpg'.format(at))
+        output = str(tmp_dir / '1_5_{}_out.jpg'.format(at))
+
+        workers.append(
+                mp.Process(target = process_fn, args=(keyword, target, output)))
+        worker_files.append((target, output))
+
+    for w in workers:
+        w.start()
+
+    for w in workers:
+        w.join()
+
+    for ((target, output), keyword) in zip(worker_files, keywords):
+        add_generated_to_canvas(c, target, output, '{} from google'.format(keyword), delta_imresize(del_lines, del_columns))
 
 
 def main():
     parser = argparse.ArgumentParser(description = 'Generate project pdf.')
-    parser.add_argument('output', type=str, help='Where to store the generated pdf')
-    parser.add_argument('--data_dir', default='./data/', type=str, help='Where to find the image data')
-
+    parser.add_argument('output', type=str, help='Where to store the generated pdf.')
+    parser.add_argument('--data_dir', default='./data/', type=str, help='Where to find the image data.')
+    parser.add_argument('--clean', default=False, type=bool, help='Clean all previously downloaded resources.')
     args = parser.parse_args()
 
     c = canvas.Canvas(args.output)
 
     data_dir = Path(args.data_dir)
 
-    tmp_dir = tempfile.TemporaryDirectory()
+    tmp_dir = Path('_work')
+    # tmp_dir = tempfile.TemporaryDirectory()
     tmp_dir_path = Path(tmp_dir.name)
+
+    if args.clean:
+        print('Cleaning previous resources.')
+        shutil.rmtree(tmp_dir_path)
+        tmp_dir.mkdir(parents=True, exist_ok=True)
 
     c.setPageSize((400, 700))
     c.drawCentredString(200, 300, 'Proiect 2 Vedere Artificiala')
     c.drawCentredString(300, 500, 'Stavarache Petru-Eric, Grupa 334')
     c.showPage()
 
-    add_task_1_1(c, tmp_dir_path, data_dir)
-    add_task_1_2(c, tmp_dir_path, data_dir)
-    add_task_1_3(c, tmp_dir_path, data_dir)
+    # add_task_1_1(c, tmp_dir_path, data_dir)
+    # add_task_1_2(c, tmp_dir_path, data_dir)
+    # add_task_1_3(c, tmp_dir_path, data_dir)
+    # add_task_1_4(c, tmp_dir_path, data_dir)
+    add_task_1_5(c, tmp_dir_path, data_dir)
 
     c.save()
 
--- Proiect2/main.py
+++ Proiect2/main.py
@@ -2,7 +2,9 @@
 
 import pdb
 import sys
+import time
 import argparse
+import random
 import matplotlib.pyplot as plt
 
 import skimage.color
@@ -43,57 +45,116 @@ def image_energy(img):
             sobel_h(grayscale)**2 + sobel_v(grayscale)**2
             ) / np.sqrt(2)
 
-def get_best_column_path(energy):
-    (n, m) = energy.shape[0:2]
-    def neighbours(i, j):
-        ret = []
-        def add_if_good(x, y):
-            def good(x, y):
-                return x >= 0 and y >= 0 and x < n and y < m
-            if good(x, y):
-                ret.append((x, y))
-        add_if_good(i - 1, j)
-        add_if_good(i - 1, j - 1)
-        add_if_good(i - 1, j + 1)
-        return ret
+def get_column_path_fn(strategy):
 
-    best = ndarray(energy.shape, dtype=energy.dtype)
-    prev = [[None for _ in range(m)] for _ in range(n)]
+    def neighbours_with_limits(n, m, i, j):
+        assert i > 0
 
-    for j in range(m):
-        best[0][j] = energy[0][j]
+        if j == 0:
+            return [(i - 1, j), (i - 1, j + 1)]
 
-    for i in range(1, n):
+        if j == m - 1:
+            return [(i - 1, j - 1), (i - 1, j)]
+
+        return [
+            (i - 1, j - 1),
+            (i - 1, j),
+            (i - 1, j + 1)
+            ]
+
+    def get_column_path_dynamicprogramming(energy):
+        (n, m) = energy.shape[0:2]
+        def neighbours(i, j):
+            return neighbours_with_limits(n, m, i, j)
+
+        best = ndarray(energy.shape, dtype=energy.dtype)
+        prev = [[None for _ in range(m)] for _ in range(n)]
+
         for j in range(m):
-            best_prev = best[i - 1][j]
-            prv = (i - 1, j)
+            best[0][j] = energy[0][j]
 
-            for (ni, nj) in neighbours(i, j):
-                if best[ni][nj] < best_prev:
-                    best_prev = best[ni][nj]
-                    prv = (ni, nj)
+        for i in range(1, n):
+            for j in range(m):
+                best_prev = best[i - 1][j]
+                prv = (i - 1, j)
 
-            best[i][j] = best_prev + energy[i][j]
-            prev[i][j] = prv
+                for nbr in neighbours(i, j):
+                    if best[nbr] < best_prev:
+                        best_prev = best[nbr]
+                        prv = nbr
 
-    starting = 0
-    for j in range(m):
-        if (best[n - 1][j] < best[n - 1][starting]):
-            starting = j
+                best[i][j] = best_prev + energy[i][j]
+                prev[i][j] = prv
 
-    def get_prev(i, j):
-        return prev[i][j]
+        starting = 0
+        for j in range(m):
+            if (best[n - 1][j] < best[n - 1][starting]):
+                starting = j
 
-    path = {}
-    def reconstruct_path(i, j):
-        path[i] = j
-        while i > 0:
-            (i, j) = get_prev(i, j)
+        def get_prev(i, j):
+            return prev[i][j]
+
+        path = {}
+        def reconstruct_path(i, j):
             path[i] = j
+            while i > 0:
+                (i, j) = get_prev(i, j)
+                path[i] = j
 
-    reconstruct_path(n - 1, starting)
+        reconstruct_path(n - 1, starting)
 
-    return path
+        return path
+
+    def get_column_path_random(energy):
+        (n, m) = energy.shape[0:2]
+        def neighbours(i, j):
+            return neighbours_with_limits(n, m, i, j)
+
+        path = {}
+        i = n - 1
+        j = random.randint(0, m - 1)
+
+        while i > 0:
+            path[i] = j
+            (i, j) = random.choice(neighbours(i, j))
+        path[i] = j
+        return path
+
+    def get_column_path_greedy(energy):
+        (n, m) = energy.shape[0:2]
+        def neighbours(i, j):
+            return neighbours_with_limits(n, m, i, j)
+
+        path = {}
+        i = n - 1
+        j = np.argmin(energy[i])
+
+        while i > 0:
+            path[i] = j
+            nbrs = neighbours(i, j)
+
+            best_nbr = None
+            for n in nbrs:
+                if best_nbr is None or energy[n] < energy[best_nbr]:
+                    best_nbr = n
+
+            (i, j) = best_nbr
+
+        path[i] = j
+        return path
+
+    if strategy == 'dynamicprogramming':
+        return get_column_path_dynamicprogramming
+    elif strategy == 'random':
+        return get_column_path_random
+    elif strategy == 'greedyd':
+        return get_column_path_greedy
+
+    assert(False) # Invalid strategy!
+
+
+
+get_best_column_path = None
 
 def remove_column_path(img, path):
     (n, m) = img.shape[0:2]
@@ -188,25 +249,22 @@ def remove_object(img, x0, y0, len_x, len_y):
         return transpose(remove_object(transpose(img), y0, x0, len_y, len_x))
 
     assert(len_y <= len_x)
-
-    energy = image_energy(img)
-
     init_len_y = len_y
 
     print('Removing an object within a bounding box of {}x{}...'.format(len_x, len_y))
-    for i in tqdm(range(init_len_y)):
-        for i in range(len_x):
-            x = x0 + i
+    for _ in tqdm(range(init_len_y)):
+        energy = image_energy(img)
 
-            # For every line on which the special rectangle appears, mark all columns not within the rectangle with infinite cost.
-            # As a result, the path found will surely pass through the rectangle.
-            energy[x][ : y0].fill(INF)
-            energy[x][y0 + len_y : ].fill(INF)
+        # For every line on which the special rectangle appears, mark all columns not within the rectangle with infinite cost.
+        # As a result, the path found will surely pass through the rectangle.
+        energy[x0 : x0 + len_x, : y0].fill(INF)
+        energy[x0 : x0 + len_x, y0 + len_y : ].fill(INF)
 
-            path = get_best_column_path(energy)
-            img = remove_column_path(img, path)
+        path = get_best_column_path(energy)
 
-            len_y -= 1
+        img = remove_column_path(img, path)
+
+        len_y -= 1
 
     return img
 
@@ -220,6 +278,9 @@ def remove_object_loop(img):
 
         [x0, y0] = [coord[2], coord[0]]
         [x1, y1] = [coord[3], coord[1]]
+
+        print([x0, y0], [x1, y1])
+
 
         (x, y) = (x0, y0)
         (len_x, len_y) = (x1 - x, y1 - y)
@@ -236,15 +297,28 @@ def main():
     parser.add_argument('output', type=str, help='Where to save the picture.')
     parser.add_argument('--columns', type=int, help='Desired change in columns. Positive means add, negative means remove.')
     parser.add_argument('--lines', type=int, help='Desired change in lines. Positive means add, negative means remove.')
-    parser.add_argument('--remove-object', action='store_true', help='Select an object to be removed.')
+
+    parser.add_argument('--path-algorithm', type=str, default='dynamicprogramming', choices=['random', 'greedy', 'dynamicprogramming'], help='Algorithm to use for path selection.')
+
+    parser.add_argument('--remove-object-fixed-coordinates', metavar=('x0', 'y0', 'x1', 'y1'), nargs=4, type=int, help='The coordinates of the rectangle to remove.')
+    parser.add_argument('--remove-object-mouse', action='store_true', help='Select the object to be removed by using the mouse.')
     args = parser.parse_args()
+
+    global get_best_column_path
+    get_best_column_path = get_column_path_fn(args.path_algorithm)
 
     img = read_image(args.target)
 
     fin_img = img
 
-    if args.remove_object:
+    if args.remove_object_mouse:
         fin_img = remove_object_loop(fin_img)
+
+    if args.remove_object_fixed_coordinates:
+        [x0, y0, x1, y1] = args.remove_object_fixed_coordinates
+        len_x = x1 - x0
+        len_y = y1 - y0
+        fin_img = remove_object(fin_img, x0, y0, len_x, len_y)
 
     if args.columns:
         if args.columns < 0:
