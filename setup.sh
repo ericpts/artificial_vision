5213271e41ca0577cb53b95dd58ccf15ae90f451
Eric Peter Stavarache <ericptst@gmail.com>
2017-12-06 17:06:50 +0200

    Update setup.sh to install python dependencies

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+git submodule update --init
+
 for i in `seq 1 3`; do
     cd Proiect$i;
     ./setup.sh;
     cd ..;
 done
+
+sudo pip3 install -r requirements.txt
+pushd lib_google_img
+sudo pip3 install -r requirements-dev.txt
+popd
