ba05ca92c5136e80ab28f5afa6ab70599b936691
Eric Peter Stavarache <ericptst@gmail.com>
2017-12-23 15:34:08 +0200

    Project 4: rename score to cross_validate

--- Proiect4/main.py
+++ Proiect4/main.py
@@ -99,7 +99,7 @@ def main():
     samples = positive_samples + negative_samples
     labels = [1] * len(positive_samples) + [-1] * len(negative_samples)
 
-    def score(samples, labels):
+    def cross_validate(samples, labels):
         X_train, X_test, y_train, y_test = train_test_split(
             samples, labels, test_size=0.4, random_state=0)
 
